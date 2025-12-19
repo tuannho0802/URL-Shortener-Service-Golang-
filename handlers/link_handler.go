ed86960b17939a545e1240ae17fb3578b59c392c
HoangTuanFT <hoangtuanft0802@gmail.com>
2025-12-19 12:50:43 +0700

    Features: short-link can be customize letters and now can display QR Code (3rd API)

--- handlers/link_handler.go
+++ handlers/link_handler.go
@@ -3,7 +3,6 @@ package handlers
 import (
 	"math/rand"
 	"net/http"
-	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/tuannho0802/URL-Shortener-Service-Golang-/models"
@@ -11,7 +10,7 @@ import (
 )
 
 // Hàm tạo chuỗi ngẫu nhiên cho ShortCode [cite: 24]
-func generateShortCode(n int) string {
+func GenerateShortCode(n int) string {
 	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 	b := make([]rune, n)
 	for i := range b {
@@ -22,32 +21,37 @@ func generateShortCode(n int) string {
 
 // 1. API Tạo link rút gọn
 func CreateShortLink(c *gin.Context) {
-	var input struct {
-		OriginalURL string `json:"original_url" binding:"required"`
-	}
+    var input struct {
+        OriginalURL string `json:"original_url"`
+        CustomAlias string `json:"custom_alias"`
+    }
+    if err := c.ShouldBindJSON(&input); err != nil {
+        c.JSON(400, gin.H{"error": "Dữ liệu không hợp lệ"})
+        return
+    }
 
-	if err := c.ShouldBindJSON(&input); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
+    var code string
+    if input.CustomAlias != "" {
+        // Check if Allias exist
+        var existing models.Link
+        if err := store.DB.Where("short_code = ?", input.CustomAlias).First(&existing).Error; err == nil {
+            c.JSON(400, gin.H{"error": "Alias này đã được sử dụng"})
+            return
+        }
+        code = input.CustomAlias
+    } else {
+        code = GenerateShortCode(6) // Generate 6 random code
+    }
 
-	// Tạo mã ngẫu nhiên 6 ký tự
-	shortCode := generateShortCode(6)
-	// Lưu ý: Thực tế cần check xem mã này đã tồn tại chưa để tránh trùng lặp [cite: 25]
+    // Save on db
+    newLink := models.Link{
+        OriginalURL: input.OriginalURL,
+        ShortCode:   code,
+        ClickCount:  0,
+    }
+    store.DB.Create(&newLink)
 
-	link := models.Link{
-		OriginalURL: input.OriginalURL,
-		ShortCode:   shortCode,
-		CreatedAt:   time.Now(),
-	}
-
-	store.DB.Create(&link)
-
-	// Trả về full URL rút gọn
-	c.JSON(http.StatusOK, gin.H{
-		"short_url": "http://localhost:8080/" + shortCode,
-		"data":      link,
-	})
+    c.JSON(200, gin.H{"short_url": "http://localhost:8080/" + code})
 }
 
 // 2. API Redirect
@@ -55,21 +59,21 @@ func RedirectLink(c *gin.Context) {
 	code := c.Param("code")
 	var link models.Link
 
-	// Tìm link trong DB theo ShortCode
+	// Find link in ShortCode
 	if err := store.DB.Where("short_code = ?", code).First(&link).Error; err != nil {
 		c.JSON(http.StatusNotFound, gin.H{"error": "Link not found"})
 		return
 	}
 
-	// Tăng lượt click [cite: 14]
+	// Update click
 	link.ClickCount++
 	store.DB.Save(&link)
 
-	// Redirect về trang gốc
+	// Redirect main page
 	c.Redirect(http.StatusFound, link.OriginalURL)
 }
 
-// 3. API Lấy danh sách link [cite: 19]
+// 3. API get list
 func GetAllLinks(c *gin.Context) {
 	var links []models.Link
 	store.DB.Find(&links)
